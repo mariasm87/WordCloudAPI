a25016c5ce458f0c65b9166679209d71f7b5bfdb
mariasm87 <60471502+mariasm87@users.noreply.github.com>
2022-07-24 16:24:17 +0200

    Revert FTP changes

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ app = FastAPI()
 @app.get("/items/")
 async def root_path(country: int = 0, fromdate: int = 10, todate: int = 10):
 
-    fileName = 'Analysis'+country+'_'+str(month)+'_'+str(partition)+'.csv'
-    ftp = FTP('gnldm1026.siteground.biz')
-    ftp.login("mariasaizmunoz@data.undp.org", "MibebeGuille_21")
-    ftp.cwd('data.undp.org/EWS')
-    #handle = open(path.rstrip("/") + "/" + filename.lstrip("/"), 'wb')
-    #ftp.retrbinary('RETR %s' % filename, handle.write)
-    ftp.quit() 
     text = "Guille Guille Guille Luci Luci Luci"
     wordcloud = WordCloud(stopwords = STOPWORDS,
                                   collocations=True,min_word_length=4,collocation_threshold=3,max_words=2).generate(text)
