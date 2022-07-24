c1a10eec294b7c4137598f96d33a3b635aff6327
mariasm87 <60471502+mariasm87@users.noreply.github.com>
2022-07-25 01:05:05 +0200

    Test2

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from wordcloud import WordCloud, STOPWORDS
 from ftplib import FTP
 import os
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/items/")
 async def root_path(country: int = 0, fromdate: int = 10, todate: int = 10):
-
+    if(country==1):
+        df = pd.read_csv('Uganda_Hate.csv')
     text = "Guille Guille Guille Luci Luci Luci"
+    text = df.clean_text[0]
     wordcloud = WordCloud(stopwords = STOPWORDS,
                                   collocations=True,min_word_length=4,collocation_threshold=3,max_words=2).generate(text)
     text_dict={k: v for k, v in sorted(wordcloud.process_text(text).items(),reverse=True, key=lambda item: item[1])}
