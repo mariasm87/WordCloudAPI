3830cf365d9f946a5ff2183db4ce5d389acb8f5f
mariasm87 <60471502+mariasm87@users.noreply.github.com>
2022-07-25 02:34:03 +0200

    test5

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ app = FastAPI()
 @app.get("/items/")
 async def root_path(country: int = 1, fromdate: str = '2022-01-01T00:00:00', todate: str = '2022-01-31T00:00:00', topic: int = 0):
     if(country==1):
-        df = pd.read_csv('Uganda_Hate.csv')
+        str_topic = "Uganda_"+str(topic)+".csv"
     elif(country==2):
-        df = pd.read_csv('Colombia_Hate.csv')
+        str_topic = "Colombia_"+str(topic)+".csv"
     elif(country==3):
-        df = pd.read_csv('Philippines_Hate.csv')
-
+        str_topic = "Philippines_"+str(topic)+".csv"
+    df = pd.read_csv(str_topic)
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%dT%H:%M:%S')
     filtered_df = df.loc[(df['date'] >= fromdate)
                      & (df['date'] < todate)]
     out = filtered_df['clean_text']
     text = "".join(str(out))
-    pattern = r'[0-9]'
 
-    new_string = re.sub(pattern, '', text)
+    new_string = re.sub(r'[0-9]', '', text)
     new_string = re.sub(r'\n', '', new_string)
+    new_string = re.sub(r'Name: clean_text, Length: , dtype: object', '', new_string)
 
     wordcloud = WordCloud(stopwords = STOPWORDS,
                                   collocations=True,min_word_length=4,collocation_threshold=3,max_words=1).generate(new_string)
