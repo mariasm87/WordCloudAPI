a9324dc78feeecbe515a9b5b47eaa6f35d11c598
mariasm87 <60471502+mariasm87@users.noreply.github.com>
2022-07-27 17:46:14 +0200

    Topic 0(all) supported

--- main.py
+++ main.py
@@ -18,12 +18,39 @@ app.add_middleware(
 @app.get("/items/")
 async def root_path(country: int = 1, fromdate: str = '2022-01-01T00:00:00', todate: str = '2022-01-31T00:00:00', topic: int = 0):
     if(country==1):
-        str_topic = "Uganda_"+str(topic)+".csv"
+        if(topic==0):
+            df1 = pd.read_csv("Uganda_1.csv")
+            df2 = pd.read_csv("Uganda_2.csv")
+            df3 = pd.read_csv("Uganda_3.csv")
+            df4 = pd.read_csv("Uganda_4.csv")
+            df5 = pd.read_csv("Uganda_5.csv")
+            df = pd.concat([df1,df2,df3,df4,df5])
+        else:
+            str_topic = "Uganda_"+str(topic)+".csv"
+            df = pd.read_csv(str_topic)
     elif(country==2):
-        str_topic = "Colombia_"+str(topic)+".csv"
+        if(topic==0):
+            df1 = pd.read_csv("Colombia_1.csv")
+            df2 = pd.read_csv("Colombia_2.csv")
+            df3 = pd.read_csv("Colombia_3.csv")
+            df4 = pd.read_csv("Colombia_4.csv")
+            df5 = pd.read_csv("Colombia_5.csv")
+            df = pd.concat([df1,df2,df3,df4,df5])
+        else:
+            str_topic = "Colombia_"+str(topic)+".csv"
+            df = pd.read_csv(str_topic)
     elif(country==3):
-        str_topic = "Philippines_"+str(topic)+".csv"
-    df = pd.read_csv(str_topic)
+        if(topic==0):
+            df1 = pd.read_csv("Philippines_1.csv")
+            df2 = pd.read_csv("Philippines_2.csv")
+            df3 = pd.read_csv("Philippines_3.csv")
+            df4 = pd.read_csv("Philippines_4.csv")
+            df5 = pd.read_csv("Philippines_5.csv")
+            df = pd.concat([df1,df2,df3,df4,df5])
+        else:
+            str_topic = "Philippines_"+str(topic)+".csv"
+            df = pd.read_csv(str_topic)
+    
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%dT%H:%M:%S')
     filtered_df = df.loc[(df['date'] >= fromdate)
                      & (df['date'] < todate)]
