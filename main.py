931744c20253954196a6ed05dbfc736a61eff062
mariasm87 <60471502+mariasm87@users.noreply.github.com>
2022-07-27 18:30:08 +0200

    testing thresholds

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ async def root_path(country: int = 1, fromdate: str = '2022-01-01T00:00:00', tod
     new_string = new_string.replace("Name: clean_text, Length: , dtype: object","")
 
     wordcloud = WordCloud(stopwords = STOPWORDS,
-                                  collocations=True,min_word_length=4,collocation_threshold=15,max_words=2).generate(new_string)
+                                  collocations=True,min_word_length=4,collocation_threshold=6,max_words=2).generate(new_string)
     text_dict={k: v for k, v in sorted(wordcloud.process_text(new_string).items(),reverse=True, key=lambda item: item[1])}
     return {"message": text_dict}
